7fd523a30d9a1cebd5f1b65cbf98fa8695abe71f
Caleb Waters <cw@mekanation.com>
2020-06-26 12:11:31 -0500

    Minor design tweaks

--- menu/Meat.java
+++ menu/Meat.java
@@ -13,7 +13,7 @@ public class Meat {
     public static final Meat chicken = new Meat("Chicken");
     public static final Meat steak = new Meat("Steak");
     public static final Meat groundBeef = new Meat("groundBeef");
-    public static final Meat beyondBurger = new Meat("BeyondBurger");
+    public static final Meat beyondBurger = new Meat("Beyond Burger");
     public static final Meat impossibleBurger = new Meat("Impossible Burger");
     public static final Meat blackBean = new Meat("Black Bean");
 
